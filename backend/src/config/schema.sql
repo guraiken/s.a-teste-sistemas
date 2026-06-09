d4fc1c198c272913f71236692899c1394b1340c7
guraiken <gustavo.fiabani@gmail.com>
2026-06-08 21:58:35 -0300

    config: added inserts to mock data

--- backend/src/config/schema.sql
+++ backend/src/config/schema.sql
@@ -26,3 +26,31 @@ CREATE TABLE carros (
     valor DECIMAL(9,2) NOT NULL,
     ano INTEGER
 );
+
+INSERT INTO carros (modelo, cor, valor, ano)
+VALUES
+('Fiat Uno', 'Vermelho', 15000, 2019),
+('Nissan Versa', 'Vermelho', 35000, 2020),
+-- Populares e Hatches
+('Chevrolet Onix', 'Branco', 55000, 2021),
+('Hyundai HB20', 'Prata', 58000, 2022),
+('Volkswagen Gol', 'Preto', 42000, 2020),
+('Renault Sandero', 'Cinza', 38000, 2019),
+('Ford Ka', 'Vermelho', 40000, 2020),
+
+-- Sedans
+('Toyota Corolla', 'Prata', 110000, 2022),
+('Honda Civic', 'Preto', 115000, 2021),
+('Chevrolet Onix Plus', 'Azul', 68000, 2023),
+('Volkswagen Virtus', 'Branco', 75000, 2022),
+
+-- SUVs e Utilitários
+('Jeep Renegade', 'Verde', 95000, 2021),
+('Hyundai Creta', 'Branco', 105000, 2022),
+('Volkswagen T-Cross', 'Cinza', 102000, 2023),
+('Nissan Kicks', 'Preto', 88000, 2021),
+
+-- Modelos Recentes / Premium
+('Fiat Fastback', 'Cinza', 125000, 2024),
+('BYD Dolphin', 'Delfim Grey', 150000, 2024),
+('GWM Ora 03', 'Branco', 160000, 2024);
